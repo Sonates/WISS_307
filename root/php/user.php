3874471671d5a52609362dffc7473b6482d2562a
Sonates <nphonyiam@outlook.com>
2022-06-08 23:18:58 +0200

    2022-06-08
    - Added & worked on jquery
    
    -worked on userform.html // Need more styling css so trigger warning get's highlighted.
    
    -Added user.php // So far it's not working. Used it with Xampp

---
+++ root/php/user.php
@@ -0,0 +1,9 @@
+<?php 
+if(isset($_POST['username'])){
+	$string = "admin";
+	$un = htmlspecialchars($_POST['username']);
+	if(stripos($string,$un)==true) echo trim("false");
+		else echo trim("true");
+}
+else die
+?>
